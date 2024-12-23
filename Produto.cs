47f0f8bf969e91407de0432d5ef2770e1f03469a
Gabriel Lacerda <127674463+lacerdagb@users.noreply.github.com>
2024-12-23 05:48:19 -0300

    Primeiro commit
    Adicionando arquivos

---
+++ Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorDeEstoque
+{
+    [System.Serializable]
+    internal abstract class Produto
+    {
+        public string nome;
+        public double preco;
+    }
+}
